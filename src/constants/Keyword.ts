59cb6d15a0c40e178e5d27caf18d5baf6d772d86
Matteo Zanoncello <matteozanoncello.work@gmail.com>
2018-10-31 17:48:04 +0100

    fix: Keyword export. added: uglify

--- src/constants/Keyword.ts
+++ src/constants/Keyword.ts
@@ -1,47 +1,53 @@
-import {assign, constant, id, obj, value} from 'core/base';
-import {UPPER_CASE} from 'core/naming';
-import {appendNode} from 'core/print';
-import resolveSyntax, {BracketsTerm, ComposedTerm, KeywordTerm, MethodTerm, Term} from 'css-syntax-parser';
-import {Mdn} from 'data/mdn';
-import {PropertiesData} from 'data/properties';
+import { assign, constant, id, obj, value } from 'core/base';
+import { UPPER_CASE } from 'core/naming';
+import { appendNode } from 'core/print';
+import resolveSyntax, { BracketsTerm, ComposedTerm, KeywordTerm, MethodTerm, Term } from 'css-syntax-parser';
+import { Mdn } from 'data/mdn';
+import { PropertiesData } from 'data/properties';
 
 export function genKeyword() {
 
-    const extractKeywords = (term: Term, extracted: string[]): string[] => {
+    const keywords: string[] = [];
+    const addKeywords = (...listOfKeyword: string[]) => {
+        listOfKeyword.map(keyword => {
+            if (!keywords.includes(keyword)) {
+                keywords.push(keyword);
+            }
+        });
+    };
+
+    const extractKeywords = (term: Term) => {
         if (term instanceof ComposedTerm) {
-            term.children.map(child => extractKeywords(child, extracted));
+            term.children.map(child => {
+                extractKeywords(child);
+            });
         }
         else if (term instanceof BracketsTerm) {
-            extractKeywords(term.content, extracted);
+            extractKeywords(term.content);
         }
         else if (term instanceof MethodTerm) {
-            extractKeywords(term.params, extracted);
+            extractKeywords(term.params);
         }
         else if (term instanceof KeywordTerm) {
             const keyword = term._value;
-            if (!extracted.includes(keyword)) {
-                extracted.push(keyword);
-            }
+            addKeywords(keyword);
         }
-        return extracted;
     };
-
-    const keywords: string[] = [];
 
     Object.keys(Mdn.Properties).map(propertyName => {
         const term = resolveSyntax(Mdn.Properties[propertyName]);
-        extractKeywords(term, keywords);
+        extractKeywords(term);
     });
 
     Object.keys(Mdn.Syntaxes).map(syntaxName => {
         const term = resolveSyntax(Mdn.Syntaxes[syntaxName]);
-        extractKeywords(term, keywords);
+        extractKeywords(term);
     });
 
     const keywordId = id('Keyword');
     const keyword = constant(keywordId,
         obj(
-            PropertiesData
+            keywords
                 .filter(keywordName => {
                     try {
                         UPPER_CASE(keywordName);
@@ -55,7 +61,7 @@ export function genKeyword() {
                 .sort()
                 .map(keywordName => {
                     let constantName = UPPER_CASE(keywordName);
-                    if (/^[A-Z]/.test(keywordName)) {
+                    if (/^[^a-z]/.test(keywordName)) {
                         constantName = '_' + constantName;
                     }
                     return assign(
